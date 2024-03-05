170ae988d8bb236858ba9467c9ca512602523d38
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-03-05 15:51:22 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from handlers import dialogue
+from handlers import handlers_start
+
+BOT_TOKEN = '6862881309:AAGb2D4bXWM7rKbnU_zzUbDp6BqneJqrDRI'
+
+async def main():
+    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_routers(handlers_start.router,dialogue.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
+  

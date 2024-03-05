25e26a6aac40e7f003efee98952eac2eb7d0cc73
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-03-05 18:14:41 +0200

    Create keyboard_users.py

---
+++ keyboard1/keyboard_users.py
@@ -0,0 +1,22 @@
+from aiogram import types
+
+buttons1 = [
+           [types.InlineKeyboardButton(text="Русский", callback_data="Русский"),],
+           [types.InlineKeyboardButton(text="English", callback_data="English"), ] ,
+           ]
+
+buttons2 = [
+           [types.InlineKeyboardButton(text="Notify the manager", callback_data="Notify_manager"),],
+           ]
+buttons3 = [
+           [types.InlineKeyboardButton(text="Сообщить менеджеру", callback_data="Сообщить менеджеру"),],
+           ]
+
+buttons4 = [
+           [types.InlineKeyboardButton(text="Начать деалог", callback_data="Начать_деалог"),],
+           ]
+
+buttons5 = [
+           [types.InlineKeyboardButton(text="Закончить диалог", callback_data="Закончить диалог"),],
+           ]
+

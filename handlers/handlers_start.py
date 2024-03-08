c133352b9a4e380bf7b615e4154a4999b5ff9fdd
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-03-08 15:55:31 +0200

    Update handlers_start.py

--- handlers/handlers_start.py
+++ handlers/handlers_start.py
@@ -10,7 +10,9 @@ router = Router()
 
 chat_id = "-1001995368517"
 
-language = 0
+dialogue = {}
+language = {}
+user_id1 = None
 
 class Form (StatesGroup):
     text1 = State()
@@ -18,13 +20,17 @@ class Form (StatesGroup):
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    if language == 0:
+    global language
+    user_id1 = msg.from_user.id
+    if user_id1 not in language:
+        language[user_id1]={'langeage1':0}
+    if language[user_id1]['langeage1'] == 0:
         keyboard1 = types.InlineKeyboardMarkup(inline_keyboard=buttons1,resize_keyboard=True,)
         await msg.answer("Choose a language", reply_markup=keyboard1)
-    elif language == 1:
+    elif language[user_id1]['langeage1'] == 1:
            keyboard2 = types.InlineKeyboardMarkup(inline_keyboard=buttons3,resize_keyboard=True,)
            await msg.answer("Если вам нужно связаться с менеджером, то нажмите на кнопку", reply_markup=keyboard2)
-    elif language == 2:
+    elif language[user_id1]['langeage1'] == 2:
           keyboard3 = types.InlineKeyboardMarkup(inline_keyboard=buttons2,resize_keyboard=True,)
           await msg.answer("If you need to contact the manager, click on the button", reply_markup=keyboard3)  
     
@@ -34,21 +40,33 @@ async def start_handler(msg: Message):
 @router.callback_query(F.data == "Русский")
 async def Russian(call: types.CallbackQuery , state: FSMContext):
     global language
-    language =+ 1
+    user_id1 = call.from_user.id
+    language[user_id1]['langeage1'] =+ 1
     await state.set_state(Form.text1)
     await call.message.edit_text("Видите ваши проблемы :")
+    
  
 @router.message(Form.text1)
 async def text_ru(msg: types.Message, state: FSMContext, bot: Bot):
+    global user_id1 , dialogue
     text1 = msg.text.strip()
     text2 = len(text1)
     if text2 < 10:
          await msg.answer("Напиши еще что-нибудь интересное")
     else:
-        user_id = msg.from_user.id
+        user_id1 = msg.from_user.id
+        print (user_id1)
+        user_id = user_id1
         name = msg.from_user.first_name
         username = msg.from_user.username
-        profile_link = f"https://t.me/{username}"
+        profile_link = f"https://t.me/{username}" 
+        if user_id1 not in dialogue:
+             dialogue[user_id1]={
+            "id_users": user_id1,
+            "id_meneger": None,
+            "name": f"{name}",
+            "status": "True",
+        }
         await state.clear()
         keyboard1 = types.InlineKeyboardMarkup(inline_keyboard=buttons4,resize_keyboard=True,)
         await bot.send_message(chat_id , text=f"{user_id} \n\n {name} \n\n Русский \n\n {profile_link} \n\n {text1}",reply_markup=keyboard1)
@@ -60,23 +78,35 @@ async def text_ru(msg: types.Message, state: FSMContext, bot: Bot):
 @router.callback_query(F.data == "English")
 async def English(call: types.CallbackQuery , state: FSMContext):
     global language
-    language =+ 2 
+    user_id1 = call.from_user.id
+    language[user_id1]['langeage1'] =+ 1
     await state.set_state(Form.text2)
     await call.message.edit_text("Write your problems :")
     
+    
 @router.message(Form.text2)
 async def text_eng(msg: types.Message, state: FSMContext, bot: Bot):
+    global dialogue ,user_id1
     text = msg.text.strip()
     text1 = len(text)
     if text1 < 10:
          await msg.answer("Write something else interesting")
     else:
-        user_id = msg.from_user.id
+        user_id1 = msg.from_user.id
         name = msg.from_user.first_name
         username = msg.from_user.username
         profile_link = f"https://t.me/{username}"
+        print(dialogue)
+        if user_id1 not in dialogue:
+             dialogue[user_id1]={
+            "id_users": user_id1,
+            "id_meneger": None,
+            "name": f"{name}",
+            "status": "True",
+        }
+        print(dialogue)     
         await state.clear()
         keyboard1 = types.InlineKeyboardMarkup(inline_keyboard=buttons4,resize_keyboard=True,)
-        await bot.send_message(chat_id , text=f"{user_id} \n\n {name} \n\n English \n\n {profile_link} \n\n{text}",reply_markup=keyboard1)
+        await bot.send_message(chat_id , text=f"{user_id1} \n\n {name} \n\n English \n\n {profile_link} \n\n{text}",reply_markup=keyboard1)
         await msg.answer("We will notify you if the manager responds to you")
             

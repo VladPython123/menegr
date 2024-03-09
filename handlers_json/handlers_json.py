281bdbd0eb82dea7f1fc93106602736f70ce3f62
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-03-09 16:31:42 +0200

    Update handlers_json.py

--- handlers_json/handlers_json.py
+++ handlers_json/handlers_json.py
@@ -11,20 +11,24 @@ router = Router()
 class Form (StatesGroup):
     ru = State()
 
-
 @router.message(Command("greeting"))
-async def start_handler(msg: Message, state: FSMContext):
-      await state.set_state(Form.ru)
-      await msg.answer("Напишите текст который будет отправляться пользователю если вы связаться с ним")
-      
+async def greeting_handler(msg: Message, state: FSMContext):
+    await state.set_state(Form.ru)
+    await msg.answer("Введите текст, который будет отправлен пользователю при связи с ним")
+
 @router.message(Form.ru)
 async def text_ru(msg: types.Message, state: FSMContext):
     text1 = msg.text.strip() 
-    user_id = msg.from_user.id
+    meneger_id = msg.from_user.id
     with open('handlers_json/meneger.json', 'r', encoding='utf-8') as file:
-       data = json.load(file)
-    data[f'{user_id}'] = f'{text1}'
+        data = json.load(file)
+    if str(meneger_id) in data:
+        data[str(meneger_id)] = text1
+        message = "Текст успешно обновлен"
+    else:
+        data[str(meneger_id)] = text1
+        message = "Новый текст успешно добавлен"
     with open('handlers_json/meneger.json', 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
     await state.clear()  
-    await msg.answer("Харашо текст был сохранён")
+    await msg.answer(message)

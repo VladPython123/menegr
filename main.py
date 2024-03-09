ecbce00affbd352c71a83bafd9d875c64ee806bd
VladPython123 <156468245+VladPython123@users.noreply.github.com>
2024-03-09 16:34:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from handlers import dialogue
 from handlers import handlers_start
 from handlers_json import handlers_json
+from admin_panely import handlers_admin
 
 BOT_TOKEN = '6862881309:AAGb2D4bXWM7rKbnU_zzUbDp6BqneJqrDRI'
 
 async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
-    dp.include_routers(handlers_start.router,dialogue.router,handlers_json.router)
+    dp.include_routers(handlers_start.router,handlers_json.router,handlers_admin.router,dialogue.router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
